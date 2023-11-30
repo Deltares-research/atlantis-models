13b57643898229a6cfc8db0a0431ff96285bbaf0
basknaake <bas.knaake@gmail.com>
2023-11-30 10:23:04 +0100

    Changes to publish project

--- atmod/download/ahn.py
+++ atmod/download/ahn.py
@@ -7,7 +7,11 @@ class AhnWcs:
     Attempt to download ahn from the WCS server. So far does not work.
 
     """
-    def __init__(self, url=r'https://service.pdok.nl/rws/ahn/wcs/v1_0?SERVICE=WCS&request=GetCapabilities'):
+
+    def __init__(
+        self,
+        url=r'https://service.pdok.nl/rws/ahn/wcs/v1_0?SERVICE=WCS&request=GetCapabilities',
+    ):
         self.wcs = WebCoverageService(url)
         print(2)
 
@@ -36,22 +40,18 @@ class AhnWcs:
             crs='EPSG:28992',
             subsets=subsets,
             width=width_pixels,
-            height=height_pixels
+            height=height_pixels,
         )
         # array = np.frombuffer(response.read(), np.float32)
         return response
 
 
 if __name__ == "__main__":
-    from atmod.build import build_template_model
-
     xmin, xmax = 120_000, 121_000
     ymin, ymax = 440_000, 441_000
     bbox = (xmin, ymin, xmax, ymax)
 
     ahn = AhnWcs()
     ahn.download_dtm(bbox)
-
-    template = build_template_model(100, 100, 126_550, 450_050, 100)
 
     print(2)
